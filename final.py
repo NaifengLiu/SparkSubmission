29bd92f8df6aa622574ce710cc7a05225e1264fe
naifengliu <naifengliu.us@gmail.com>
2020-05-18 16:29:15 -0400

    hw3 attempt 6

--- final.py
+++ final.py
@@ -7,10 +7,10 @@ if __name__=='__main__':
     sc = SparkContext()
     spark = SparkSession(sc)
     df = spark.read.csv("/data/share/bdm/nyc_parking_violation/2015.csv",
-                        header=True, multiLine=True, escape='"')
+                        header=True, multiLine=True)
 
     df = df.select(df['Violation County'], df['House Number'], df['Street Name'])
-    d = df.collect(5).saveAsTextFile('1')
+    df.write.csv('test')
 
 
 
