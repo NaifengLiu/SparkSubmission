14c833c8c61ade9d2466f38d974ab2bfc05c2fc9
naifengliu <naifengliu.us@gmail.com>
2020-05-18 16:41:05 -0400

    hw3 attempt 6

--- final.py
+++ final.py
@@ -2,40 +2,17 @@ from pyspark import SparkContext
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as sf
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     sc = SparkContext()
     spark = SparkSession(sc)
     df = spark.read.csv("/data/share/bdm/nyc_parking_violation/2015.csv",
-                        header=True, multiLine=True)
+                        header=True, multiLine=True, escape='"')
 
     df = df.select(df['Violation County'], df['House Number'], df['Street Name'])
-    df.write.csv('test')
+    df.write.csv('2015')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    df = spark.read.csv("/data/share/bdm/nyc_cscl.csv",
+                        header=True, multiLine=True, escape='"')
+    df = df.select(df['PHYSICALID'], df['FULL_STREET'], df['ST_NAME'], df['L_LOW_HN'], df['L_HIGH_HN'], df['R_LOW_HN'],
+                   df['R_HIGH_HN'])
+    df.write.csv('cscl')
