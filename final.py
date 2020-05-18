1f4e5d2a4ae98ea10e6d9d73480a6b4ddb5cebd5
naifengliu <naifengliu.us@gmail.com>
2020-05-18 16:06:56 -0400

    hw3 attempt 6

--- final.py
+++ final.py
@@ -6,12 +6,11 @@ import pyspark.sql.functions as sf
 if __name__=='__main__':
     sc = SparkContext()
     spark = SparkSession(sc)
-    # df = spark.read.csv("hdfs:///data/share/bdm/nyc_parking_violation/2015.csv",
-    #                     header=True, multiLine=True, escape='"')
-    #
-    # df = df.select(df[])
-    with open("/data/share/bdm/nyc_parking_violation/2015.csv") as f:
-        print(f.readline())
+    df = spark.read.csv("hdfs:///data/share/bdm/nyc_parking_violation/2015.csv",
+                        header=True, multiLine=True, escape='"')
+
+    df = df.select(df['Violation County'], df['House Number'], df['Street Name'])
+    df.collect(5)
 
 
 
