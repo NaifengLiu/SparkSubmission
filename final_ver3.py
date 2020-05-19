a381a8ed9ae3a074d2c057e2027caafddfb7a53b
naifengliu <naifengliu.us@gmail.com>
2020-05-19 17:30:24 -0400

    hw3 attempt 6

--- final_ver3.py
+++ final_ver3.py
@@ -18,7 +18,7 @@ def find_id(house, street, street_dict):
     return None
 
 
-def process(pid, records):
+def process(records):
     import csv
     from ast import literal_eval
 
@@ -26,19 +26,19 @@ def process(pid, records):
 
     counts = {}
 
+    streets_list = dict()
+
     boros = [0, dict(), dict(), dict(), dict(), dict()]
     boro_id = [1,2,3,4,5]
 
     for single_id in boro_id:
-        with open(SparkFiles.get("boro_" + str(single_id) + ".csv")) as f:
+        with open("boro_" + str(single_id) + ".csv") as f:
             for line in f.readlines():
                 street_name = line.split(",")[0]
                 rest = ','.join(line.split(",")[1:])
                 boros[single_id][street_name] = literal_eval(rest)
             f.close()
 
-    if pid == 0:
-        next(records)
     for row in reader:
         county = row[0]
         num = row[1]
@@ -66,16 +66,16 @@ if __name__ == '__main__':
 
     # path = "hdfs:///data/share/bdm/nyc_cscl.csv"
     # path = "hdfs:///user/nliu/boros/boro_1.csv"
-    sc.addFile("hdfs:///user/nliu/boros/boro_1.csv")
-    sc.addFile("hdfs:///user/nliu/boros/boro_2.csv")
-    sc.addFile("hdfs:///user/nliu/boros/boro_3.csv")
-    sc.addFile("hdfs:///user/nliu/boros/boro_4.csv")
-    sc.addFile("hdfs:///user/nliu/boros/boro_5.csv")
+    # sc.addFile("hdfs:///user/nliu/boros/boro_1.csv")
+    # sc.addFile("hdfs:///user/nliu/boros/boro_2.csv")
+    # sc.addFile("hdfs:///user/nliu/boros/boro_3.csv")
+    # sc.addFile("hdfs:///user/nliu/boros/boro_4.csv")
+    # sc.addFile("hdfs:///user/nliu/boros/boro_5.csv")
 
-    df = spark.read.csv("/data/share/bdm/nyc_parking_violation/2015.csv", header=True, multiLine=True, escape='"')
+    df = spark.read.csv("2015.csv", header=True, multiLine=True, escape='"')
 
     rdd = df.select(df['Violation County'], df['House Number'], df['Street Name']).rdd
 
-    counts = rdd.mapPartitionsWithIndex(process).reduceByKey(lambda x, y: x + y).collect()
+    counts = rdd.mapPartitions(process).reduceByKey(lambda x, y: x + y).collect()
 
     counts.saveAsTextFile("2222")
